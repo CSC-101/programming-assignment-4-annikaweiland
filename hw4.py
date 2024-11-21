3a71881eea8946d734986fd25cd0680085997524
annikaweiland <amweilan@calpoly.edu>
2024-11-20 23:00:26 -0800

    Programming Assignment 4 Submission - Annika Weiland

--- hw4.py
+++ hw4.py
@@ -4,171 +4,6 @@ import county_demographics
 import data
 import os
 
-reduced_data = [
-    data.CountyDemographics(
-        {'Percent 65 and Older': 13.8,
-         'Percent Under 18 Years': 25.2,
-         'Percent Under 5 Years': 6.0},
-        'Autauga County',
-        {"Bachelor's Degree or Higher": 20.9,
-         'High School or Higher': 85.6},
-        {'American Indian and Alaska Native Alone': 0.5,
-         'Asian Alone': 1.1,
-         'Black Alone': 18.7,
-         'Hispanic or Latino': 2.7,
-         'Native Hawaiian and Other Pacific Islander Alone': 0.1,
-         'Two or More Races': 1.8,
-         'White Alone': 77.9,
-         'White Alone, not Hispanic or Latino': 75.6},
-        {'Per Capita Income': 24571,
-         'Persons Below Poverty Level': 12.1,
-         'Median Household Income': 53682},
-        {'2010 Population': 54571,
-         '2014 Population': 55395,
-         'Population Percent Change': 1.5,
-         'Population per Square Mile': 91.8},
-        'AL'),
-    data.CountyDemographics(
-        {'Percent 65 and Older': 15.3,
-         'Percent Under 18 Years': 25.1,
-         'Percent Under 5 Years': 6.0},
-        'Crawford County',
-        {"Bachelor's Degree or Higher": 14.3,
-         'High School or Higher': 82.2},
-        {'American Indian and Alaska Native Alone': 2.5,
-         'Asian Alone': 1.6,
-         'Black Alone': 1.6,
-         'Hispanic or Latino': 6.7,
-         'Native Hawaiian and Other Pacific Islander Alone': 0.1,
-         'Two or More Races': 2.8,
-         'White Alone': 91.5,
-         'White Alone, not Hispanic or Latino': 85.6},
-        {'Per Capita Income': 19477,
-         'Persons Below Poverty Level': 20.2,
-         'Median Household Income': 39479},
-        {'2010 Population': 61948,
-         '2014 Population': 61697,
-         'Population Percent Change': -0.4,
-         'Population per Square Mile': 104.4},
-        'AR'),
-    data.CountyDemographics(
-        {'Percent 65 and Older': 17.5,
-         'Percent Under 18 Years': 18.1,
-         'Percent Under 5 Years': 4.8},
-        'San Luis Obispo County',
-        {"Bachelor's Degree or Higher": 31.5,
-         'High School or Higher': 89.6},
-        {'American Indian and Alaska Native Alone': 1.4,
-         'Asian Alone': 3.8,
-         'Black Alone': 2.2,
-         'Hispanic or Latino': 22.0,
-         'Native Hawaiian and Other Pacific Islander Alone': 0.2,
-         'Two or More Races': 3.4,
-         'White Alone': 89.0,
-         'White Alone, not Hispanic or Latino': 69.5},
-        {'Per Capita Income': 29954,
-         'Persons Below Poverty Level': 14.3,
-         'Median Household Income': 58697},
-        {'2010 Population': 269637,
-         '2014 Population': 279083,
-         'Population Percent Change': 3.5,
-         'Population per Square Mile': 81.7},
-        'CA'),
-    data.CountyDemographics(
-        {'Percent 65 and Older': 11.5,
-         'Percent Under 18 Years': 21.7,
-         'Percent Under 5 Years': 5.8},
-        'Yolo County',
-        {"Bachelor's Degree or Higher": 37.9,
-         'High School or Higher': 84.3},
-        {'American Indian and Alaska Native Alone': 1.8,
-         'Asian Alone': 13.8,
-         'Black Alone': 3.0,
-         'Hispanic or Latino': 31.5,
-         'Native Hawaiian and Other Pacific Islander Alone': 0.6,
-         'Two or More Races': 5.0,
-         'White Alone': 75.9,
-         'White Alone, not Hispanic or Latino': 48.3},
-        {'Per Capita Income': 27730,
-         'Persons Below Poverty Level': 19.1,
-         'Median Household Income': 55918},
-        {'2010 Population': 200849,
-         '2014 Population': 207590,
-         'Population Percent Change': 3.4,
-         'Population per Square Mile': 197.9},
-        'CA'),
-    data.CountyDemographics(
-        {'Percent 65 and Older': 19.6,
-         'Percent Under 18 Years': 25.6,
-         'Percent Under 5 Years': 4.9},
-        'Butte County',
-        {"Bachelor's Degree or Higher": 17.9,
-         'High School or Higher': 89.2},
-        {'American Indian and Alaska Native Alone': 1.0,
-         'Asian Alone': 0.3,
-         'Black Alone': 0.2,
-         'Hispanic or Latino': 5.8,
-         'Native Hawaiian and Other Pacific Islander Alone': 0.2,
-         'Two or More Races': 2.3,
-         'White Alone': 96.1,
-         'White Alone, not Hispanic or Latino': 90.6},
-        {'Per Capita Income': 20995,
-         'Persons Below Poverty Level': 15.7,
-         'Median Household Income': 41131},
-        {'2010 Population': 2891,
-         '2014 Population': 2622,
-         'Population Percent Change': -9.4,
-         'Population per Square Mile': 1.3},
-        'ID'),
-    data.CountyDemographics(
-        {'Percent 65 and Older': 15.3,
-         'Percent Under 18 Years': 25.1,
-         'Percent Under 5 Years': 6.9},
-        'Pettis County',
-        {"Bachelor's Degree or Higher": 15.2,
-         'High School or Higher': 81.8},
-        {'American Indian and Alaska Native Alone': 0.7,
-         'Asian Alone': 0.7,
-         'Black Alone': 3.4,
-         'Hispanic or Latino': 8.3,
-         'Native Hawaiian and Other Pacific Islander Alone': 0.3,
-         'Two or More Races': 1.9,
-         'White Alone': 92.9,
-         'White Alone, not Hispanic or Latino': 85.5},
-        {'Per Capita Income': 19709,
-         'Persons Below Poverty Level': 18.4,
-         'Median Household Income': 38580},
-        {'2010 Population': 42201,
-         '2014 Population': 42225,
-         'Population Percent Change': 0.1,
-         'Population per Square Mile': 61.9},
-        'MO'),
-    data.CountyDemographics(
-        {'Percent 65 and Older': 18.1,
-         'Percent Under 18 Years': 21.6,
-         'Percent Under 5 Years': 6.5},
-        'Weston County',
-        {"Bachelor's Degree or Higher": 17.2,
-         'High School or Higher': 90.2},
-        {'American Indian and Alaska Native Alone': 1.7,
-         'Asian Alone': 0.4,
-         'Black Alone': 0.7,
-         'Hispanic or Latino': 4.2,
-         'Native Hawaiian and Other Pacific Islander Alone': 0.0,
-         'Two or More Races': 2.2,
-         'White Alone': 95.0,
-         'White Alone, not Hispanic or Latino': 91.5},
-        {'Per Capita Income': 28764,
-         'Persons Below Poverty Level': 11.2,
-         'Median Household Income': 55461},
-        {'2010 Population': 7208,
-         '2014 Population': 7201,
-         'Population Percent Change': -0.1,
-         'Population per Square Mile': 3.0},
-        'WY')
-    ]
-
-
 #TASK 1
 #Main code below operations
 #INPUTS: a single command-line argument (with an 'operations' file)
@@ -177,8 +12,7 @@ reduced_data = [
 
 #OPERATIONS
 
-#FIX DISPLAY FUNCTION
-def display(data: dict) -> dict:
+def display(data: dict) -> str:
     for county in data:
         print ("COUNTY, ST: {}, {} \n \t AGES: {} \n \t EDUCATION: {} \n \t ETHNICITIES: {} \n \t EDUCATION: {}".format(county.county, county.state, county.age, county.education, county.ethnicities, county.income))
     if data == []:
@@ -199,11 +33,8 @@ def filter_gt(data, field: str, num = float) -> list:
     filter_gt_list = []
     for county in data:
         attribute_top = getattr(county, param1, None)
-        if param2 not in attribute_top:
-            return("ERROR WITH PARAMETER GIVEN")
-        elif param2 in attribute_top:
-            if attribute_top[param2] > num:
-                filter_gt_list.append(county)
+        if attribute_top[param2] > num:
+            filter_gt_list.append(county)
     entries = len(filter_gt_list)
     data = filter_gt_list
     print("Filter: {} gt {}% ({} entries)".format(field, num, entries))
@@ -216,11 +47,8 @@ def filter_lt(data, field: str, num: float) -> list:
     filter_lt_list = []
     for county in data:
         attribute_top = getattr(county, param1, None)
-        if param2 not in attribute_top:
-            return("ERROR WITH PARAMETER GIVEN")
-        elif param2 in attribute_top:
-            if attribute_top[param2] < num:
-                filter_lt_list.append(county)
+        if attribute_top[param2] < num:
+            filter_lt_list.append(county)
     entries = len(filter_lt_list)
     data = filter_lt_list
     print("Filter: {} lt {}% ({} entries)".format(field, num, entries))
@@ -239,19 +67,14 @@ def population_field(data, field: str) -> float:
     param2 = field_list[1]
     for county in data:
         attribute_top = getattr(county, param1, None)
-        if param2 not in attribute_top:
-            return("ERROR WITH PARAMETER GIVEN")
-        if param2 in attribute_top:
-            pop_portion+= county.population['2014 Population']*(attribute_top[param2])/100
-    print("2014 {} population: {}".format(field, pop_portion))
-
+        pop_portion+= county.population['2014 Population']*(attribute_top[param2])/100
     return pop_portion
 
-def percent_field(data, field) -> (float):
+def percent_field(data, field) -> float:
     tot = population_total(data)
     if tot == 0:
         print("the given data set is blank.")
-        return("the given data set is blank")
+        return 0
     try:
         tot_portion = population_field(data, field)
         percentage = (tot_portion/tot)*100
@@ -272,11 +95,17 @@ def opening_data_and_file(op_file: str, data = list[dict]): #output is the FILE 
 
     except FileNotFoundError as e:
         return e
+
+    print(op_file)
     print("Number of initial entries: {}".format(len(data)))
 
     for line in lines:
-        liney = line.strip("\n")
+        line1 = line.strip("\n")
+        liney = line.strip()
         line_list = liney.split(":")
+
+        if not liney:
+            continue
 
         try:
             if len(line_list) == 3:
@@ -301,7 +130,9 @@ def opening_data_and_file(op_file: str, data = list[dict]): #output is the FILE 
 
         elif line_list[0] == "population":
             try:
-                population_field(data, line_list[1])
+                pop = population_field(data, line_list[1])
+                print("2014 {} population: {}".format(line_list[1], pop))
+
             except:
                 print("ERROR ON LINE <{}>".format(line))
                 continue
@@ -350,13 +181,28 @@ def opening_data_and_file(op_file: str, data = list[dict]): #output is the FILE 
             print(display(data))
         else:
             print("ERROR ON LINE <{}>".format(line))
-    return "-"
+    return "all file lines processed\n"
+
+
+#EXECUTION OF ALL FILES:
 
 if __name__ == '__main__':
-
-    print(opening_data_and_file(sys.argv[1], reduced_data))
-
-
+# pop.ops: name in command line
+    print(opening_data_and_file(sys.argv[1], build_data.get_data()))
+#pop_field.ops
+    print(opening_data_and_file("pop_field.ops", build_data.get_data()))
+#percent_fields.ops
+    print(opening_data_and_file("percent_fields.ops", build_data.get_data()))
+#high_school_lt_60.ops
+    print(opening_data_and_file("high_school_lt_60.ops", build_data.get_data()))
+#filter_state.ops
+    print(opening_data_and_file("filter_state.ops", build_data.get_data()))
+#ca.ops
+    print(opening_data_and_file("ca.ops", build_data.get_data()))
+#bachelors_gt_60.ops
+    print(opening_data_and_file("bachelors_gt_60.ops", build_data.get_data()))
+#some_errors.ops
+    print(opening_data_and_file("some_errors.ops", build_data.get_data()))
 #TASK 2:
 #Operations files: task2 a, b, c, and d
 
