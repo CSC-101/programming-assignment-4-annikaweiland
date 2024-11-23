c642b7ffe610a5705bdffc92b953586a5aaba0b3
annikaweiland <amweilan@calpoly.edu>
2024-11-22 21:05:40 -0800

    Programming Assignment 4 Submission - Annika Weiland

--- hw4.py
+++ hw4.py
@@ -11,22 +11,30 @@ import os
 #PURPOSE
 
 #OPERATIONS
-
-def display(data: dict) -> str:
+#input: A list of data type CountyDemographics
+#output: A string of all counties in the given data list
+#purpose: To display the counties in data in a user-friendly manner
+def display(data: list[data.CountyDemographics]) -> str:
     for county in data:
         print ("COUNTY, ST: {}, {} \n \t AGES: {} \n \t EDUCATION: {} \n \t ETHNICITIES: {} \n \t EDUCATION: {}".format(county.county, county.state, county.age, county.education, county.ethnicities, county.income))
     if data == []:
         print("There are no counties in this dataset.")
     exit(1)
 
-def filter_state(data, input_st: str) -> list:
+#input: A list containing CountyDemographics, and a given str that is the input state.
+#output: A str stating the amount of entries
+#purpose: To find out how many of the county demographics have the input state as their state
+def filter_state(data: list[data.CountyDemographics], input_st: str) -> list:
     filtered_list = [county for county in data if county.state == input_st]
     data = filtered_list
     amt_entries = len(data)
     print ("Filter: state == {} ({} entries)".format(input_st, amt_entries))
     return data
 
-def filter_gt(data, field: str, num = float) -> list:
+#input: A list containing CountyDemographics, a field that is a str, and a float number.
+#output: A list of CountyDemographics that fit the given parameters.
+#purpose: To filter the data list by the given field percentages that are greater than the given percentage value.
+def filter_gt(data: list[data.CountyDemographics], field: str, num = float) -> list:
     field_list = field.split(".")
     param1 = field_list[0].lower()
     param2 = field_list[1]
@@ -40,7 +48,10 @@ def filter_gt(data, field: str, num = float) -> list:
     print("Filter: {} gt {}% ({} entries)".format(field, num, entries))
     return data
 
-def filter_lt(data, field: str, num: float) -> list:
+#input: A list containing CountyDemographics, a field that is a str, and a float number.
+#output:  A list of CountyDemographics that fit the given parameters.
+#purpose: To filter the data list by the given field percentages that are greater than the given percentage value.
+def filter_lt(data: list[data.CountyDemographics], field: str, num: float) -> list:
     field_list = field.split(".")
     param1 = field_list[0].lower()
     param2 = field_list[1]
@@ -54,13 +65,19 @@ def filter_lt(data, field: str, num: float) -> list:
     print("Filter: {} lt {}% ({} entries)".format(field, num, entries))
     return data
 
-def population_total(data: list[dict]) -> int:
+#input: A list containing CountyDemographics
+#output: An integer value representative of the total population of the list of counties.
+#purpose: To determine the total population of the given data set.
+def population_total(data: list[data.CountyDemographics]) -> int:
     total_pop = 0
     for county in data:
         total_pop += county.population['2014 Population']
     return total_pop
 
-def population_field(data, field: str) -> float:
+#input: A list containing CountyDemographics, and a given field that is a str.
+#output: A float value representative of a population.
+#purpose: To return the total population in a given field in a given list of counties.
+def population_field(data: list[data.CountyDemographics], field: str) -> float:
     pop_portion = 0
     field_list = field.split(".")
     param1 = field_list[0].lower()
@@ -70,7 +87,10 @@ def population_field(data, field: str) -> float:
         pop_portion+= county.population['2014 Population']*(attribute_top[param2])/100
     return pop_portion
 
-def percent_field(data, field) -> float:
+#input: A list of data type CountyDemographics and a field that is a str.
+#output: A float value that is representative of the percentage in the given population of a given field.
+#purpose: To determine the percent of a given field in the given population of counties in the data.
+def percent_field(data: list[data.CountyDemographics], field: str) -> float:
     tot = population_total(data)
     if tot == 0:
         print("the given data set is blank.")
@@ -85,7 +105,10 @@ def percent_field(data, field) -> float:
 
 
 #Main code:
-def opening_data_and_file(op_file: str, data = list[dict]): #output is the FILE BEING OPENED
+#input: A str that is the name of a file (or a command-line argument containing a str that is the name of a file), and a list of data type CountyDemographics
+#output: A string value.
+#purpose: To open and read the given op file, and perform the given functions in op file on the parameter data
+def opening_data_and_file(op_file: str, data = list[data.CountyDemographics]) -> str:
 #input would be from command line - sys.argv[1] - op_file
 #data is build_data.get_data() at first, but updates depending on the operation
     try:
@@ -187,23 +210,9 @@ def opening_data_and_file(op_file: str, data = list[dict]): #output is the FILE 
 #EXECUTION OF ALL FILES:
 
 if __name__ == '__main__':
-# pop.ops: name in command line
+#change file in command line to test out different files
     print(opening_data_and_file(sys.argv[1], build_data.get_data()))
-#pop_field.ops
-    print(opening_data_and_file("pop_field.ops", build_data.get_data()))
-#percent_fields.ops
-    print(opening_data_and_file("percent_fields.ops", build_data.get_data()))
-#high_school_lt_60.ops
-    print(opening_data_and_file("high_school_lt_60.ops", build_data.get_data()))
-#filter_state.ops
-    print(opening_data_and_file("filter_state.ops", build_data.get_data()))
-#ca.ops
-    print(opening_data_and_file("ca.ops", build_data.get_data()))
-#bachelors_gt_60.ops
-    print(opening_data_and_file("bachelors_gt_60.ops", build_data.get_data()))
-#some_errors.ops
-    print(opening_data_and_file("some_errors.ops", build_data.get_data()))
-#TASK 2:
+
 #Operations files: task2 a, b, c, and d
 
 
